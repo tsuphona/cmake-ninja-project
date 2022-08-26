a03632af0c5c90290a642500a15fb13a9d504ed9
tsuphona <thomas.suphona@gmail.com>
2022-08-26 19:31:29 +0200

    add submodule glfw

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <adder.hpp>
 
 int main()
 {
-    std::cout << "CMake is an awesome tool!" << std::endl;
+    std::cout << "CMake is an awesome tool!\n";
+    std::cout << "Ninja is also an awesome tool!\n";
+    std::cout << add(6.5f, 5.5f) << "\n";
+
     return 0;
 }
