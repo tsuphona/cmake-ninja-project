5a7993571c06fffc2054bfb1a56608dc68c87476
tsuphona <thomas.suphona@gmail.com>
2022-08-28 16:50:43 +0200

    Add library options

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <GLFW/glfw3.h>
-#include <adder.hpp>
 #include <cnpConfig.h>
 #include <iostream>
+#ifdef USE_ADDER
+  #include <adder.hpp>
+#endif
 
 int main(int argc, char *argv[]) {
   std::cout << "CMake is an awesome tool!\n";
   std::cout << "Ninja is also an awesome tool!\n";
-  std::cout << add(6.5f, 5.5f) << "\n";
+
+#ifdef USE_ADDER
+  std::cout << "Using Adder lib: " << add(6.5f, 5.5f) << "\n";
+#else
+  std::cout << "Not using Adder lib: " << 6.5f + 5.5f << "\n";
+#endif
 
   std::cout << argv[0] << " Version " << cnp_VERSION_MAJOR << "."
             << cnp_VERSION_MINOR << "\n";
